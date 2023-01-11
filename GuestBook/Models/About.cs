5e243cea3fbe0bddbffc0804cc0a9d0c1ae0123b
beaabrahamsson <beaabrahamsson@hotmail.com>
2023-01-11 20:42:31 +0100

    Update About.cs

--- GuestBook/Models/About.cs
+++ GuestBook/Models/About.cs
@@ -10,7 +10,7 @@ namespace GuestBook.Models
     {
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string MoreInfoUrl => "https://github.com/beaabrahamsson/DT071G-Projekt-beaab2100";
         public string Message => "Detta är en Gästbok skapad för kursen Programmering i C#.NET. Appen är skapad med XAML och C# med .NET MAUI. För mer information samt källkod, se länk nedan.";
     }
 }
